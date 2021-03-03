01774ed8905b98525e5fe3f6a5cc2af67fe19034
davisantosr <davi-rocha2@hotmail.com>
2021-03-03 19:21:46 -0300

    types #1

---
+++ exercise_01.dart
@@ -0,0 +1,14 @@
+main() {
+  print('Hello, World!');
+  int a = 3;
+  double b = 2.2;
+  bool isRaining = true;
+  bool isCold = false;
+
+  // var c = "Hello";
+  // c = 1;
+  print(a);
+  print(isRaining);
+
+  print(isRaining || isCold);
+}
