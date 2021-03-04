24635106cb93f171535c778b719b09b9a42dddf7
davisantosr <davi-rocha2@hotmail.com>
2021-03-03 22:50:16 -0300

    Function | Class | constructors | named parameters |

--- exercise_03.dart
+++ exercise_03.dart
@@ -1,12 +1,42 @@
+somePrint({required String name, double price = 9.99}) {
+  print('the name is: ${name} and the price is \$${price}');
+}
+
 class Product {
   String name = '';
-  double price = 0;
+  double price = 0.00;
+
+  Product(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  // Product(this.name, this.price); => handy constructor
+  // Product({this.name = '', this.price = 9.99}); named parameters
 }
 
 main() {
-  var p1 = new Product();
-  p1.name = 'Pencil';
-  p1.price = 4.30;
+  var p1 = new Product('Pencil', 4.50);
+  var p2 = Product('Fridge', 99.99);
 
-  print('The product: ${p1.name} is \$${p1.price}');
+  print('Product: ${p1.name}');
+  print('Price: \$${p1.price}');
+
+  print('Product: ${p2.name}');
+  print('Price: \$${p2.price}');
+
+  somePrint(name: 'Car', price: 3750.00);
 }
+
+// class Product {
+//   String name = '';
+//   double price = 0;
+// }
+
+// main() {
+//   var p1 = new Product();
+//   p1.name = 'Pencil';
+//   p1.price = 4.30;
+
+//   print('The product: ${p1.name} is \$${p1.price}');
+// }
